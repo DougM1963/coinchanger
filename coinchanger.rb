08fd55ea4de23f872c55b1caee2492e74b042eae
dougm1963 <dougmeador1963@gmail.com>
2018-01-10 20:26:06 -0500

    initial add

---
+++ coinchanger.rb
@@ -0,0 +1,4 @@
+def get_change(cents_received)
+    coins_returned = {}
+    coins = {"quarter" => 25, "dime" => 10, "nickel" => 5, "penny" => 1}
+ end
---
+++ coinchangertest.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require_relative "coinchanger.rb"
+
+class TestMeme < Minitest::Test
+def test_100_equals_4_quarter
+    assert_equal(4, 4)
+    end
+end
